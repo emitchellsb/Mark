a1f3e59e5effb6e45bc8518bf17eecfa63cb4863
ethanmdev <z5162062@unsw.edu.au>
2017-12-08 10:58:46 +1100

    update

--- caller.py
+++ caller.py
@@ -3,12 +3,15 @@ from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 from google.oauth2 import service_account
-import pyttsx
+from PyDictionary import PyDictionary
+from gtts import gTTS
 import os
 import sys
 import subprocess
 import pyaudio
 import wave
+import string
+import time
 
 def record():
 	CHUNK = 1024
@@ -49,7 +52,7 @@ def record():
 
 def transcribe_file(speech_file):
     # Transcribe the given audio file.
-    credentials = service_account.Credentials.from_service_account_file('settings/Mark-341e0729698c.json')
+    credentials = service_account.Credentials.from_service_account_file('./settings/Mark-341e0729698c.json')
 
     scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
 
@@ -65,22 +68,84 @@ def transcribe_file(speech_file):
         language_code='en-US') #CHECK THIS
 
     response = client.recognize(config, audio)
-    # Print the first alternative of all the consecutive results.
-    #for result in response.results:
-        #print('Transcript: {}'.format(result.alternatives[0].transcript))
     
     return response
 
-def tts(response):
+def isLike(keywords, text): #keywords is an ORDERED array of strings, input is the text input of the user
+	likeness = 0
+	text = text.translate(None, string.punctuation)
+	text = text.split(" ")
+	for word in text:
+		word = word.lower
+	for word in keywords:
+		word = word.lower
+
+	num_contains = 0
+	included_arr = []
+	for word in keywords:
+		synonyms = PyDictionary.synonym(word)
+		if(synonyms == None):
+			synonyms = []
+		else:
+			for synonym in synonyms:
+				synonym = str(synonym)
+		synonyms.extend([word, word+'s'])	
+		for synonym in synonyms:
+			if (synonym in text):
+				included_arr.append(word)
+				num_contains += 1
+				break
+	contains_rating = int(1000*num_contains/len(keywords))
+
+	used_keywords = []
+	for word in keywords:
+		if (word in included_arr):
+			used_keywords.append(word)
+	
+	if(len(used_keywords) > 1):	
+		total_precedes = 0
+		i = len(used_keywords) - 1
+		while (i >= 1):
+			total_precedes += i
+			i -= 1
+		num_precedes = 0
+		length = len(used_keywords)
+		i = 0
+		while (i < length):
+			word1 = used_keywords[i]
+			j = i+1
+			while (j < length):
+				word2 = used_keywords[j]
+				if (included_arr.index(word1) < included_arr.index(word2)):
+					num_precedes += 1
+				j += 1
+			i += 1
+
+		order_rating = int(1000*num_precedes/total_precedes)
+
+		likeness = float((2*contains_rating + order_rating)/300)
+
+	return likeness
+
+def get_response(response):
 	transcript = ''
 	for result in response.results:
-		transcript += format(result.alternatives[0].transcript) 
+		transcript += format(result.alternatives[0].transcript)
+	return transcript
 
-	print(transcript)
+def decide(text):
+	likeness = isLike(['what', 'time'], text)
+	if (likeness >= 8.0):
+		return 'The current time is ' + time.ctime()
+	
+	else: 
+		return 'cleverbot'
 
-	engine = pyttsx.init()
-	engine.say(transcript)
-	engine.runAndWait()
+def tts(output):
+
+	tts = gTTS(text=output, lang='en-au')
+	tts.save('output.mp3')
+	os.system('mpg321 output.mp3')
 
 def main():
 
@@ -88,15 +153,19 @@ def main():
 	filename = "input.wav"
 
 	while(1):
-		# Step 1: Call java program to get audio input
+		# Step 1: Get audio input
 		record()
 		# Step 2: Call transcribe_file function to make API request
 		response = transcribe_file(filename)
 
-		# Step 3: Read text response and confidence level
-		tts(response)
+		# Step 3: Read text response
+		text = str(get_response(response))
+		print(text)
 
 		# Step 4: Decide on action and response
+		decision = decide(text)
+		
+		tts(decision)
 		# Step 5: Perform action
 		# Step 6: Respond
 		# Step 7: Clear data (delete audio file, etc)
