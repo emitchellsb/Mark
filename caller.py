fcf72cc0f6afc721294738a5b6e094c63a3ddc5b
ethanmdev <z5162062@unsw.edu.au>
2017-12-08 14:21:26 +1100

    adds advanced news features

--- caller.py
+++ caller.py
@@ -12,6 +12,7 @@ import pyaudio
 import wave
 import string
 import time
+import requests
 
 def record():
 	CHUNK = 1024
@@ -65,11 +66,17 @@ def transcribe_file(speech_file):
     config = types.RecognitionConfig(
         encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
         sample_rate_hertz=44100,
-        language_code='en-US') #CHECK THIS
+        language_code='en-US')
 
     response = client.recognize(config, audio)
     
     return response
+
+def get_response(response):
+	transcript = ''
+	for result in response.results:
+		transcript += format(result.alternatives[0].transcript)
+	return transcript
 
 def isLike(keywords, text): #keywords is an ORDERED array of strings, input is the text input of the user
 	likeness = 0
@@ -122,24 +129,165 @@ def isLike(keywords, text): #keywords is an ORDERED array of strings, input is t
 			i += 1
 
 		order_rating = int(1000*num_precedes/total_precedes)
+	else: 
+		order_rating = 1000
 
-		likeness = float((2*contains_rating + order_rating)/300)
+	likeness = float((2*contains_rating + order_rating)/300)
 
 	return likeness
 
-def get_response(response):
-	transcript = ''
-	for result in response.results:
-		transcript += format(result.alternatives[0].transcript)
-	return transcript
+def news(text):
+	text = text.translate(None, string.punctuation)
+	text = text.split(" ")
+	for word in text:
+		word = word.lower
+
+	source_index = ['abc-news', 'abc-news-au', 'aftenposten', 'al-jazeera-english', 'ansa', 'argaam', 'ars-technica', 'ary-news', 'associated-press', 'australian-financial-review', 'axios', 'bbc-news', 'bbc-sport', 'bild', 'blasting-news-br', 'bleacher-report', 'bloomberg', 'breitbart-news', 'buisness-insider', 'business-insider-uk', 'buzzfeed', 'cbc-news', 'cbs-news', 'cnbc', 'cnn', 'cnn-es', 'crypto-coins-news', 'daily-mail', 'der-tagesspiegel', 'die-ziet', 'el-mundo', 'engadget', 'entertainment-weekly', 'espn', 'espn-cric-info', 'financial-post', 'financial-times', 'focus', 'footbal-italia', 'fortune', 'four-four-two', 'fox-news', 'fox-sports', 'globo', 'google-news', 'google-news-ar', 'google-news-au', 'google-news-br', 'google-news-ca', 'google-news-fr', 'google-news-in', 'google-news-is', 'google-news-it', 'google-news-ru', 'google-news-sa', 'google-news-uk', 'goteborgs-posten', 'gruenderszene', 'hacker-news', 'handelsblatt', 'ign', 'il-sole-24-ore', 'independent', 'infobae', 'info-money', 'la-gaceta', 'la-nacion', 'la-repubblica', 'le-monde', 'lenta', 'lequipe', 'les-echos', 'liberation', 'marca', 'mashable', 'medical-news-today', 'metro', 'mirror', 'msnbc', 'mtv-news', 'mtv-news-uk', 'national-geographic', 'nbc-news', 'news24', 'new-scientist', 'news-com-au', 'newsweek', 'new-york-magazine', 'next-big-future', 'nfl-news', 'nhl-news', 'nrk', 'politico', 'polygon', 'rbc', 'recode', 'reddit-r-all', 'reuterse', 'rt', 'rte', 'rtl-nieuws', 'sabq', 'spiegel-online', 'svenska-dagbladet', 't3n', 'talksport', 'techcrunch', 'techcrunch-cn', 'techradar', 'the-economist', 'the-globe-and-mail', 'the-guardian-au', 'the-guardian-uk', 'the-hill', 'the-hindu', 'the-huffington-post', 'the-irish-times', 'the-lad-bible', 'the-new-york-times', 'the-next-web', 'the-sport-bible', 'the-telegraph', 'the-times-of-india', 'the-verge', 'the-wall-street-journal', 'the-washington-post', 'time', 'usa-today', 'vice-news', 'wired', 'wired-de', 'wirtschafts-woche', 'xinhua-net', 'ynet']
+	source_index_searchable = []
+	for source in source_index:
+		source_index_searchable.append(source.replace('-', ' '))
+	
+	source_tag = 'bbc-news'
+	news_key = '379dd265f19d4465ae7a77cb30c2684f'
+	source = 'bbc news'
+	q = ''
+	articles = 'The top 4 trending stories are: '
+	num = 4
+	max_num = 4
+	desc_type = 0
+
+	if ('from' in text or 'by' in text): #we have specified a source
+		if ('on' in text or 'about' in text):
+			desc_type = 3
+			try:
+				src_index = text.index('from')
+			except:
+				src_index = text.index('by')
+			try:
+				tpc_index = text.index('on')
+			except:
+				tpc_index = text.index('about')
+			if (src_index < tpc_index): #news from __ on __
+				i = src_index + 1
+				if (i != tpc_index):
+					source = ''
+					while (i < tpc_index-1):
+						source += text[i]
+						i += 1
+					#check source is real
+					for source_s in source_index_searchable:
+						if (source_s.find(source.lower()) != -1):
+							source_tag = source_index[source_index_searchable.index(source_s)]
+							break
+				i = tpc_index + 1
+				topic = ''
+				if (i != len(text)):
+					while (i < len(text)):
+						topic += text[i]
+						i += 1
+					q = '&q='+topic
+
+			else: #news on __ from __
+				topic = ''
+				i = tpc_index + 1
+				if (i != src_index):
+					while (i < tpc_index-1):
+						topic += text[i]
+						i += 1
+					q = '&q='+topic
+				i = src_index + 1
+				if (i != len(text)):
+					source = ''
+					while (i < len(text)):
+						source += text[i]
+						i += 1
+				#check source is real
+				for source_s in source_index_searchable:
+					if (source_s.find(source.lower()) != -1):
+						source_tag = source_index[source_index_searchable.index(source_s)]
+						break
+
+		else:
+			#just source
+			desc_type = 1
+			try:
+				index = text.index('from')
+			except:
+				index = text.index('by')
+			i = index+1
+			if (i != len(text)):
+				source = ''
+				while (i < len(text)):
+					source += text[i]
+					i += 1
+				#check source is real
+				for source_s in source_index_searchable:
+					if (source_s.find(source.lower()) != -1):
+						source_tag = source_index[source_index_searchable.index(source_s)]
+						break
+
+	elif ('on' in text or 'about' in text):
+		#just topic
+		desc_type = 2
+		try:
+			index = text.index('on')
+		except:
+			index = text.index('about')
+		topic = ''
+		q = ''
+		i = index+1
+		if (i != len(text)):
+			while (i < len(text)):
+				topic += text[i]
+				i += 1
+			q += '&q='+topic
+
+
+	url = ('https://newsapi.org/v2/top-headlines?sources='
+       +source_tag+q+'&apiKey='+news_key)
+	print(url)
+
+	news = requests.get(url).json()
+
+	if(news['status'] == 'ok'):
+		if (desc_type != 0):
+			num = 0
+			news_read = ''
+			for article in news['articles']:
+				if(num < max_num):
+					news_read += article['title'] + '. '
+					num += 1
+				else:
+					break
+			if (num != 0):
+				if (desc_type == 1):
+					articles = 'The top '+str(num)+' trending stories by '+source+' are: '+news_read
+				elif (desc_type == 2):
+					articles = 'The top '+str(num)+' trending stories about '+topic+' are: '+news_read
+				elif (desc_type == 3):
+					articles = 'The top '+str(num)+' trending stories by '+source+' about '+topic+' are: '+news_read
+			else:
+				articles = "I didn't find any articles, sorry mate."
+
+		else:
+			i = 0
+			while(i < num):
+				articles += news['articles'][i]['title'] + '. '
+				i += 1
+	else:
+		articles = "I didn't find any articles, sorry mate."
+
+	return articles
 
 def decide(text):
 	likeness = isLike(['what', 'time'], text)
 	if (likeness >= 8.0):
 		return 'The current time is ' + time.ctime()
-	
-	else: 
-		return 'cleverbot'
+	likeness = isLike(['news'], text)
+	if (likeness >= 8.0):
+		return news(text)
+
+	return 'cleverbot'
 
 def tts(output):
 
@@ -165,6 +313,8 @@ def main():
 		# Step 4: Decide on action and response
 		decision = decide(text)
 		
+		print('DECISION: ' + decision)
+
 		tts(decision)
 		# Step 5: Perform action
 		# Step 6: Respond
